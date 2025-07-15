e03317d629f0a35423f647eb44144a42be712e7f
JP2899 <jesuspiedra1999@gmail.com>
2025-07-14 20:14:58 -0600

    Cuadrado.java

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,1 +1,17 @@
+package negocio.figuras;
 
+public class Cuadrado extends Rectangulo {
+    public Cuadrado(double lado) {
+        super(lado, lado);
+        this.nombre = "Cuadrado";
+    }
+
+    public double getLado() {
+        return getBase(); // En un cuadrado, base = altura = lado
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Lado: " + getLado() + ")";
+    }
+}
