0462e091d99b69646522dbd988a33ac95553121d
ear14 <edgar.retana.mora@gmail.com>
2025-07-20 08:46:25 -0600

    Coleccion creation

---
+++ src/cr/cenfotec/BL/Coleccion.java
@@ -0,0 +1,7 @@
+public class Coleccion{
+    //Importaciones y paquetes
+    import java.util.ArrayList;
+    //Atributos
+    ArrayList <Cuerpo> coleccionCuerpos = new ArrayList<>;
+    ArrayList <Figura> coleccionFiguras = new ArrayList<>;
+}
