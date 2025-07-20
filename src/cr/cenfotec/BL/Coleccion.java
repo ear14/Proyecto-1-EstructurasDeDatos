c2b230af7b7344d95908c07ba4e8d443c135270f
ear14 <edgar.retana.mora@gmail.com>
2025-07-20 11:53:33 -0600

    Coleccion

--- src/cr/cenfotec/BL/Coleccion.java
+++ src/cr/cenfotec/BL/Coleccion.java
@@ -15,17 +15,27 @@ public class Coleccion{
     }
 
     public void listarFiguras(){
-        for (String figura : this.coleccionFiguras){
-            System.out.println(figura.get(i).toString());
+        for (Figura figura : this.coleccionFiguras){
+            System.out.println("Figura #" + figura.indexOf(i) + ":" + figura.toString());
         }
     }
 
     public void listarCuerpos(){
-        
+        for (Cuerpo figura : this.coleccionFiguras){
+            System.out.println("Figura #" + figura.indexOf(i) + ":" + figura.toString());
+        }
     }
 
     public void calcularAreaTotal(){
-
+        double areaFigura;
+        double areaCuerpo;
+        for (Figura figura : this.coleccionFiguras){
+                    areaFigura = figura.calcularArea(); 
+        }
+        for (Cuerpo cuerpo : this.coleccionCuerpos){
+                    areaCuerpo = cuerpo.calcularArea(); 
+        }
+        System.out.println()
     }
 
     public void calcularPerimetroTotal(){
