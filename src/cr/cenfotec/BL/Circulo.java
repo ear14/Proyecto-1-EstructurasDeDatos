50166c4d95b543b424f87d1deaf750f65aa3ffea
JP2899 <jesuspiedra1999@gmail.com>
2025-07-20 14:08:51 -0600

    Circulo.java

---
+++ src/cr/cenfotec/BL/Circulo.java
@@ -0,0 +1,37 @@
+package cr.cenfotec.BL;
+
+public class Circulo extends Figura {
+    private float radio;
+
+    public Circulo(float radio) {
+        super("Circulo");
+        this.radio = radio;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Circulo otro = (Circulo) obj;
+        return Float.compare(otro.radio, radio) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Circulo [radio=%.2f]", radio);
+    }
+}
